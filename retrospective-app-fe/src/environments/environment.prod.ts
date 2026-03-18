2d31e524fb3a809761141aeef272ac907ea3da90
Yachii9 <johnreinvinuya19@gmail.com>
2026-03-18 13:46:54 +0800

    Changed the environment to use the deployed API

--- retrospective-app-fe/src/environments/environment.prod.ts
+++ retrospective-app-fe/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: ' http://localhost:3000/api'
-  // apiBaseUrl: ' https://retrospective-app-be.vercel.app/api'
+  apiBaseUrl: ' https://retrospective-app-be.vercel.app/api'
 };
