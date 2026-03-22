79c5270f8b9f9c63fd57fe21221c535764960381
Yachii9 <johnreinvinuya19@gmail.com>
2026-03-22 16:47:58 +0800

    Fixed the typo on the API Base URL for deployment

--- retrospective-app-fe/src/environments/environment.development.ts
+++ retrospective-app-fe/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: ' https://retrospective-app-be.vercel.app/api'
+  apiBaseUrl: 'https://retrospective-app-be.vercel.app/api'
+  // apiBaseUrl: 'http://localhost:3000/api'
 };
--- retrospective-app-fe/src/environments/environment.prod.ts
+++ retrospective-app-fe/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: ' https://retrospective-app-be.vercel.app/api'
+  apiBaseUrl: 'https://retrospective-app-be.vercel.app/api'
+  // apiBaseUrl: 'http://localhost:3000/api'
 };
